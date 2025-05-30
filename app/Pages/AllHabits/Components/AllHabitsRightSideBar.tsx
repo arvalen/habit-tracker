89599f6c5f767568dc6c88a28376e97288f3cc15
arvalen <aruanvalentino@gmail.com>
2025-05-30 11:20:12 +0700

    feat: create user profile section and statistics component in right sidebar

--- app/Pages/AllHabits/Components/AllHabitsRightSideBar.tsx
+++ app/Pages/AllHabits/Components/AllHabitsRightSideBar.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+import UserProfile from "./RightSideBar/UserProfile";
+import MainStatistics from "./RightSideBar/MainStatistics";
+
+
 function AllHabitsRightSideBar() {
     return (
-        <div className="w-[20%] h-full bg-white p-5">
-            <h2 className="text-xl font-semibold mb-4">Right Sidebar</h2>
-            <p className="text-gray-600">This is the right sidebar content.</p>
-            {/* Add more content or components as needed */}
+        <div className=" w-[30%] flex flex-col items-center bg-white ">
+            <UserProfile />
+            <MainStatistics />
         </div>
     );
 }
